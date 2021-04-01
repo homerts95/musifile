e1df6bb2825fb026b0389a147278a270f5c16ae0
Homer <h.tsintilas@gmail.com>
2021-04-01 15:06:34 +0300

    thats the frontend

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1>hello sdf</h1>
+      <div className="form">
+        <label>Username</label>
+        <input type="text" name="username"/>
+        <label>password</label>
+        <input type="text" name="password"/>
+        <button>login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
